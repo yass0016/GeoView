02f77ed9b89f553148319349d07f889d1623acc0
Saleh Yassin <yass0016@gmail.com>
2021-02-08 22:15:17 -0500

    Updates

---
+++ plugins/panel-content/panel.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const PanelContent = () => {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <p>Hello {count}</p>
+            <button type="button" onClick={() => setCount(count + 1)}>
+                Add count
+            </button>
+        </div>
+    );
+};
+
+export default PanelContent;
