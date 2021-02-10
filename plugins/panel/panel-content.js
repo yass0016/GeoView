226c43928b1401ba3ccf9bb2983d0b73354fbf60
Saleh Yassin <yass0016@gmail.com>
2021-02-10 14:26:18 -0500

    Updates

--- plugins/panel/panel-content.js
+++ plugins/panel/panel-content.js
@@ -1,14 +1,56 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const PanelContent = () => {
-    const [count, setCount] = useState(0);
+    const [vectors, setVectors] = useState([]);
+
+    const { cgpv } = window;
+
+    const addVector = (vector) => {
+        setVectors([
+            ...vectors,
+            {
+                id: vector.id,
+                layer: vector.layer,
+                type: vector.type,
+                mapId: vector.handlerName,
+            },
+        ]);
+    };
+
+    const deleteVector = (id, mapId) => {
+        cgpv.api.map(mapId).deleteGeometry(id);
+
+        setVectors(
+            vectors.filter((vector) => {
+                return vector.id !== id;
+            })
+        );
+    };
+
+    useEffect(() => {
+        cgpv.api.on('vector/added', (payload) => {
+            addVector(payload);
+        });
+
+        // turn off event listeners on component unmounts
+        return () => {
+            cgpv.api.off('vector/added');
+        };
+    });
 
     return (
         <div>
-            <p>Count is: {count}</p>
-            <button type="button" onClick={() => setCount(count + 1)}>
-                Add count
-            </button>
+            {vectors.map((vector) => {
+                return (
+                    <div key={vector.id}>
+                        <p>Vector ID: {vector.id}</p>
+                        <p>Vector Type: {vector.type}</p>
+                        <button type="button" onClick={() => deleteVector(vector.id)}>
+                            Delete vector
+                        </button>
+                    </div>
+                );
+            })}
         </div>
     );
 };
