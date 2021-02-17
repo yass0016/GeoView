0aa0075c77f7438b3e07001932c246e3cf39c0eb
Saleh Yassin <yass0016@gmail.com>
2021-02-17 14:59:17 -0500

    Updates

--- plugins/basemap/basemap-switcher.js
+++ plugins/basemap/basemap-switcher.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 
-import { useTranslation } from 'react-i18next';
+import { useTranslation, getInitialProps } from 'react-i18next';
 
 const useStyles = makeStyles((theme) => ({
     card: {
@@ -12,20 +12,30 @@ const useStyles = makeStyles((theme) => ({
         '&:hover': {
             boxShadow: '0 8px 16px 0 rgba(0, 0, 0, 0.2)',
         },
+        marginBottom: 10,
+        height: '120px',
+        width: '100%',
+        display: 'block',
+        position: 'relative',
     },
     thumbnail: {
-        borderRadius: '5px 5px 0 0',
-        height: '200px',
+        position: 'absolute',
+        height: 'inherit',
+        width: 'inherit',
     },
     container: {
         background: 'rgba(0,0,0,.68)',
         color: '#fff',
         overflow: 'hidden',
         textOverflow: 'ellipsis',
-        height: '30px',
+        height: '5px',
         padding: '16px 8px 16px 16px',
         display: 'flex',
         alignItems: 'center',
+        position: 'absolute',
+        left: 0,
+        bottom: 0,
+        width: 'inherit',
     },
 }));
 
@@ -44,48 +54,110 @@ const BasemapSwitcher = (props) => {
     // get a reference to the viewer api
     const { cgpv } = window;
 
-    // get the mapId passed in when loading the component
-    const { mapId } = props;
+    // get the mapId and language passed in when loading the component
+    const { mapId, language } = props;
+    console.log('here');
+
+    const createBasemap = (id, basemapProps) => {
+        // check if basemap with provded ID exists
+        const exists = basemapList.filter((basemap) => basemap.id === id);
+
+        // if basemap does not exist then create a new one
+        if (exists.length === 0) {
+            const basemap = { ...basemapProps, id };
+
+            // create the basemap
+            cgpv.api.map(mapId).createBasemap(basemap);
+        }
+    };
+
+    const setBasemap = (id) => {
+        cgpv.api.map(mapId).setBasemap(id);
+    };
 
     useEffect(() => {
         const { basemaps } = cgpv.api.map(mapId);
 
         setBasemapList(basemaps);
 
-        // check if basemap with provded ID exists
-        const exists = basemaps.filter((basemap) => basemap.id === 'testBasemap');
-
-        // if basemap does not exist then create a new one
-        if (exists.length === 0) {
-            const basemap = {
-                id: 'testBasemap',
-                name: 'Test Basemap',
-                description: 'Test basemap',
-                descSummary: '',
-                altText: '',
-                thumbnailUrl: '',
-                layers: {},
-                attribution: 'test attribution',
-                zoomLevels: {
-                    min: 0,
-                    max: 0,
+        createBasemap('transportWithLabels', {
+            name: 'Transport with Labels',
+            description:
+                'This Canadian basemap provides geographic context with bilingual labels and an emphasis on transportation networks. From Natural Resources Canada.',
+            descSummary: '',
+            altText: 'Transport with labels',
+            thumbnailUrl: '',
+            layers: [
+                {
+                    id: 'transport',
+                    type: 'transport',
+                    url:
+                        'https://geoappext.nrcan.gc.ca/arcgis/rest/services/BaseMaps/CBMT_CBCT_GEOM_3978/MapServer/WMTS/tile/1.0.0/CBMT_CBCT_GEOM_3978/default/default028mm/{z}/{y}/{x}.jpg',
+                    opacity: 1,
                 },
-            };
+                {
+                    id: 'label',
+                    layerType: 'label',
+                    url: 'https://geoappext.nrcan.gc.ca/arcgis/rest/services/BaseMaps/xxxx_TXT_3978/MapServer/WMTS/tile/1.0.0/xxxx_TXT_3978/default/default028mm/{z}/{y}/{x}.jpg'.replaceAll(
+                        'xxxx',
+                        language === 'en-CA' ? 'CBMT' : 'CBCT'
+                    ),
+                    opacity: 0.75,
+                },
+            ],
+            attribution: 'test attribution',
+            zoomLevels: {
+                min: 0,
+                max: 0,
+            },
+        });
 
-            // create the basemap
-            cgpv.api.map(mapId).createBasemap(basemap);
-        }
+        createBasemap('transportWithNoLabels', {
+            name: 'Transport without labels',
+            description:
+                'This Canadian basemap provides geographic context that emphasis on transportation networks. From Natural Resources Canada.',
+            descSummary: '',
+            altText: 'Transport without labels',
+            thumbnailUrl: '',
+            layers: [
+                {
+                    id: 'transport',
+                    type: 'transport',
+                    url:
+                        'https://geoappext.nrcan.gc.ca/arcgis/rest/services/BaseMaps/CBMT_CBCT_GEOM_3978/MapServer/WMTS/tile/1.0.0/CBMT_CBCT_GEOM_3978/default/default028mm/{z}/{y}/{x}.jpg',
+                    opacity: 1,
+                },
+            ],
+            attribution: 'test attribution',
+            zoomLevels: {
+                min: 0,
+                max: 0,
+            },
+        });
     }, []);
 
     return (
         <div>
             {basemapList.map((basemap) => {
                 return (
-                    <div key={basemap.id}>
-                        <div className={classes.card}>
+                    <div
+                        role="button"
+                        tabIndex="0"
+                        className={classes.card}
+                        onClick={() => setBasemap(basemap.id)}
+                        onKeyPress={() => setBasemap(basemap.id)}
+                        key={basemap.id}
+                    >
+                        {typeof basemap.thumbnailUrl === 'string' && (
                             <img src={basemap.thumbnailUrl} alt={basemap.altText} className={classes.thumbnail} />
-                            <div className={classes.container}>{basemap.name}</div>
-                        </div>
+                        )}
+
+                        {Array.isArray(basemap.thumbnailUrl) &&
+                            basemap.thumbnailUrl.map((thumbnail) => {
+                                return <img src={thumbnail} alt={basemap.altText} className={classes.thumbnail} />;
+                            })}
+
+                        <div className={classes.container}>{basemap.name}</div>
                     </div>
                 );
             })}
