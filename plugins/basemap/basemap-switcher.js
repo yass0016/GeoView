985d9a20eeaf0f3638a24700ad10fad4006ee1d0
Saleh Yassin <yass0016@gmail.com>
2021-02-18 16:26:04 -0500

    Updates

--- plugins/basemap/basemap-switcher.js
+++ plugins/basemap/basemap-switcher.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 
-import { useTranslation, getInitialProps } from 'react-i18next';
-
 const useStyles = makeStyles((theme) => ({
+    listContainer: {
+        overflowY: 'scroll',
+        height: '600px',
+    },
     card: {
         boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
         transition: '0.3s',
@@ -52,15 +54,18 @@ const BasemapSwitcher = (props) => {
 
     const classes = useStyles();
 
-    const { t } = useTranslation();
-
     // get a reference to the viewer api
     const { cgpv } = window;
 
     // get the mapId and language passed in when loading the component
     const { mapId, language } = props;
-    console.log('here');
 
+    /**
+     * Create a new basemap
+     *
+     * @param {string} id the id of the basemap to be created
+     * @param {Object} basemapProps basemap properties
+     */
     const createBasemap = (id, basemapProps) => {
         // check if basemap with provded ID exists
         const exists = basemapList.filter((basemap) => basemap.id === id);
@@ -74,15 +79,26 @@ const BasemapSwitcher = (props) => {
         }
     };
 
+    /**
+     * Update the basemap with the layers on the map
+     *
+     * @param {string} id update the basemap on the map
+     */
     const setBasemap = (id) => {
         cgpv.api.map(mapId).setBasemap(id);
     };
 
+    /**
+     * load existing basemaps and create new basemaps
+     */
     useEffect(() => {
+        // get existing basemaps
         const { basemaps } = cgpv.api.map(mapId);
 
+        // set the basemaps in the list
         setBasemapList(basemaps);
 
+        // create a new basemap with transport and label layers
         createBasemap('transportWithLabels', {
             name: 'Transport with Labels',
             description:
@@ -100,7 +116,7 @@ const BasemapSwitcher = (props) => {
                 },
                 {
                     id: 'label',
-                    layerType: 'label',
+                    type: 'label',
                     url: 'https://geoappext.nrcan.gc.ca/arcgis/rest/services/BaseMaps/xxxx_TXT_3978/MapServer/WMTS/tile/1.0.0/xxxx_TXT_3978/default/default028mm/{z}/{y}/{x}.jpg'.replaceAll(
                         'xxxx',
                         language === 'en-CA' ? 'CBMT' : 'CBCT'
@@ -115,6 +131,7 @@ const BasemapSwitcher = (props) => {
             },
         });
 
+        // create a new basemap with only transport layer
         createBasemap('transportWithNoLabels', {
             name: 'Transport without labels',
             description:
@@ -137,10 +154,99 @@ const BasemapSwitcher = (props) => {
                 max: 0,
             },
         });
+
+        // create a new basemap with shaded relief layer
+        createBasemap('shadedRelief', {
+            name: 'Shaded Relief',
+            description: '":"This Canadian base map provides geographic context using shaded relief. From Natural Resources Canada.',
+            descSummary: '',
+            altText: 'Shaded Relief',
+            thumbnailUrl: '',
+            layers: [
+                {
+                    id: 'shaded',
+                    type: 'shaded',
+                    url:
+                        'https://geoappext.nrcan.gc.ca/arcgis/rest/services/BaseMaps/CBME_CBCE_HS_RO_3978/MapServer/WMTS/tile/1.0.0/CBMT_CBCT_GEOM_3978/default/default028mm/{z}/{y}/{x}.jpg',
+                    opacity: 1,
+                },
+            ],
+            attribution: 'test attribution',
+            zoomLevels: {
+                min: 0,
+                max: 0,
+            },
+        });
+
+        // create a new basemap with shaded relief and labels layer
+        createBasemap('shadedLabel', {
+            name: 'Shaded Relief with Labels',
+            description:
+                '":"This Canadian base map provides geographic context using shaded relief with labels. From Natural Resources Canada.',
+            descSummary: '',
+            altText: 'Shaded Relief with Labels',
+            thumbnailUrl: '',
+            layers: [
+                {
+                    id: 'shaded',
+                    type: 'shaded',
+                    url:
+                        'https://geoappext.nrcan.gc.ca/arcgis/rest/services/BaseMaps/CBME_CBCE_HS_RO_3978/MapServer/WMTS/tile/1.0.0/CBMT_CBCT_GEOM_3978/default/default028mm/{z}/{y}/{x}.jpg',
+                    opacity: 1,
+                },
+                {
+                    id: 'label',
+                    type: 'label',
+                    url: 'https://geoappext.nrcan.gc.ca/arcgis/rest/services/BaseMaps/xxxx_TXT_3978/MapServer/WMTS/tile/1.0.0/xxxx_TXT_3978/default/default028mm/{z}/{y}/{x}.jpg'.replaceAll(
+                        'xxxx',
+                        language === 'en-CA' ? 'CBMT' : 'CBCT'
+                    ),
+                    opacity: 1,
+                },
+            ],
+            attribution: 'test attribution',
+            zoomLevels: {
+                min: 0,
+                max: 0,
+            },
+        });
+
+        // create a new basemap with shaded relief and labels layer
+        createBasemap('shadedLabel1', {
+            name: 'Shaded Relief with Labels',
+            description:
+                '":"This Canadian base map provides geographic context using shaded relief with labels. From Natural Resources Canada.',
+            descSummary: '',
+            altText: 'Shaded Relief with Labels',
+            thumbnailUrl: '',
+            layers: [
+                {
+                    id: 'shaded',
+                    type: 'shaded',
+                    url:
+                        'https://geoappext.nrcan.gc.ca/arcgis/rest/services/BaseMaps/CBME_CBCE_HS_RO_3978/MapServer/WMTS/tile/1.0.0/CBMT_CBCT_GEOM_3978/default/default028mm/{z}/{y}/{x}.jpg',
+                    opacity: 1,
+                },
+                {
+                    id: 'label',
+                    type: 'label',
+                    url: 'https://geoappext.nrcan.gc.ca/arcgis/rest/services/BaseMaps/xxxx_TXT_3978/MapServer/WMTS/tile/1.0.0/xxxx_TXT_3978/default/default028mm/{z}/{y}/{x}.jpg'.replaceAll(
+                        'xxxx',
+                        language === 'en-CA' ? 'CBMT' : 'CBCT'
+                    ),
+                    opacity: 1,
+                },
+            ],
+            attribution: 'test attribution',
+            zoomLevels: {
+                min: 0,
+                max: 0,
+            },
+        });
     }, []);
 
     return (
-        <div>
+        <div className={classes.listContainer}>
             {basemapList.map((basemap) => {
                 return (
                     <div
@@ -156,8 +262,8 @@ const BasemapSwitcher = (props) => {
                         )}
 
                         {Array.isArray(basemap.thumbnailUrl) &&
-                            basemap.thumbnailUrl.map((thumbnail) => {
-                                return <img src={thumbnail} alt={basemap.altText} className={classes.thumbnail} />;
+                            basemap.thumbnailUrl.map((thumbnail, index) => {
+                                return <img key={index} src={thumbnail} alt={basemap.altText} className={classes.thumbnail} />;
                             })}
 
                         <div className={classes.container}>{basemap.name}</div>
